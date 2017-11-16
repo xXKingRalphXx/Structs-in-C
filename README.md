c5e48579e8a9b2f15188ee574c893ada5249dcc1
xXKingRalphXx <d.babalola@rocketmail.com>
2017-11-15 22:05:28 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+I was interested in seeing how C-stlye structures look at the assembly level, so I wrote a simple binary, and reversed it in IDA.
+
+The writeup can be found in writeup.txt, and the source code of the binary can be found in sauce.m  
+No binary is included. Compile it yourself!
+
+~KingCyrin
---
+++ sauce.m
@@ -0,0 +1,33 @@
+#import <Foundation/Foundation.h>
+#include <stdlib.h>
+
+// A 32 byte struct of four unsigned 8 byte integers
+struct reStuff {
+    uint64_t uno;
+    uint64_t dos;
+    uint64_t tres;
+    uint64_t fo;
+};
+
+// Returns the struct back to the caller
+struct reStuff structHandler(struct reStuff lolz) {
+    return lolz;
+}
+
+int main(void){
+    @autoreleasepool {
+        
+        // Declare a struct
+        struct reStuff spamm;
+        
+        // Initializes the struct's values
+        spamm.uno = 0x1337133713371337;
+        spamm.dos = 0xbadbabebadbabe00;
+        spamm.tres = 0xdeadbeefdeadbeef;
+        spamm.fo = 0xbadc0debadc0de00;
+        
+        // self explanatory
+        structHandler(spamm);
+    }
+    return 0;
+}
